d64f99f0ef5723e7541a063542c2427a4a93b773
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-03-15 20:05:07 +0900

    Modify

--- inspect.sh
+++ inspect.sh
@@ -3,12 +3,12 @@ set -x
 
 cd ./repos
 git clone --depth=1 git@github.com:fastladder/fastladder.git
-git clone --depth=1 git@github.com:redmine/redmine.git
-git clone --depth=1 git@github.com:spree/spree.git
-git clone --depth=1 git@github.com:rubygems/rubygems.org.git
-git clone --depth=1 git@github.com:alphagov/whitehall.git
-git clone --depth=1 git@github.com:lodge/lodge.git
 git clone --depth=1 git@github.com:gitlabhq/gitlabhq.git
+git clone --depth=1 git@github.com:lodge/lodge.git
+git clone --depth=1 git@github.com:redmine/redmine.git
+git clone --depth=1 git@github.com:rubygems/rubygems.org.git
+git clone --depth=1 git@github.com:spree/spree.git
+git clone --depth=1 git@github.com:alphagov/whitehall.git
 cd -
 
 set -xe
---
+++ summary.rb
@@ -0,0 +1,44 @@
+#!/opt/rubies/ruby-2.2.1/bin/ruby
+require 'pathname'
+
+dirs = %w(
+fastladder
+gitlabhq
+lodge
+redmine
+rubygems.org
+spree
+whitehall
+)
+
+h = {}
+
+path = Pathname.new File.dirname(__FILE__)
+
+dirs.each do |dir|
+  chunks = (path + dir + '.rubocop_todo.yml').read.split("\n\n")
+  chunks.shift
+  chunks.each do |chunk|
+    chunk =~ /(^[A-Z][\s\S]+):\n/
+    type = $1
+    h[type] ||= {}
+    h[type][dir] ||= {}
+
+    chunk =~ /^# Offense count: (\d+)/
+    h[type][dir][:count] = $1
+
+    match = chunk =~ /^# Cop supports --auto-correct\./
+    h[type][dir][:auto_correct] = !!match
+  end
+end
+
+puts '|' + dirs.join('|') + '|'
+puts '|' + dirs.map{':---'}.join('|') + '|'
+h.each do |k, v|
+  print "|#{k}"
+  dirs.each do |dir|
+    print "|#{(v[dir] || {})[:count]}"
+  end
+  puts "|"
+end
+
