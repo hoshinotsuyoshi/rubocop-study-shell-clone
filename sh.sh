44945b01bce218015c619948d4418b4ce160b1ca
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2015-03-15 17:50:20 +0900

    Modify sh.sh

--- sh.sh
+++ sh.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -xe
+set -x
 
 cd ./repos
 git clone --depth=1 git@github.com:fastladder/fastladder.git
@@ -10,4 +10,20 @@ git clone --depth=1 git@github.com:alphagov/whitehall.git
 git clone --depth=1 git@github.com:lodge/lodge.git
 git clone --depth=1 git@github.com:gitlabhq/gitlabhq.git
 cd -
+
+set -xe
+BUNDLE=$HOME/.gem/ruby/2.2.1/bin/bundle
+for project in fastladder redmine spree rubygems.org whitehall lodge gitlabhq
+do
+  cd ./repos/$project
+  rm -f .rubocop.yml
+  rm -rf ./.bundle
+  echo "source 'https://rubygems.org'" > Gemfile
+  echo "gem 'rubocop'" >> Gemfile
+  #$BUNDLE -j4
+  $BUNDLE exec rubocop --auto-gen-config || true
+  mkdir -p ../../$project
+  mv .rubocop_todo.yml ../../$project
+  cd -
+done
 
